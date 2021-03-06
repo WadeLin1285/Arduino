142318ca96d9a2f85df1b246ea0ba38db5b033ef
Wade Lin <wadeclin@gmail.com>
2021-03-06 22:08:23 +0800

    Add LCD_example.ino
    Add a new LAC example Arduino Program to github

---
+++ Basic Programs for Test and Examples/LCD_example/LCD_example.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h> 
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  // initialize the LCD
+  lcd.begin();
+
+  // Turn on the blacklight and print a message.
+  lcd.backlight();
+  lcd.print("Hello, world!");
+}
+
+void loop()
+{
+  // Do nothing here...
+}
--- Basic Programs for Test and Examples/LCD_with_Orbital_Matrix/LCD_with_Orbital_Matrix.ino
+++ Basic Programs for Test and Examples/LCD_with_Orbital_Matrix/LCD_with_Orbital_Matrix.ino
@@ -1,10 +1,27 @@
 #include <Wire.h>
 
-int address = 40;
+int address = 20;
 
 void setup() {
-  Wire.begin();
   Serial.begin(9600);
+  Serial.println("Starting...");
+  Wire.begin();
+  // LCD setting
+  Wire.beginTransmission(address);
+  Wire.write(254);
+  Wire.write(88);  // clean all
+  Wire.write(254);
+  Wire.write(74);  // underline on
+  Wire.write(254);
+  Wire.write(83);  // blinking on
+  Wire.endTransmission();
+  // Starting
+  Wire.beginTransmission(address);
+  Wire.write(254);
+  Wire.write(88);
+  Wire.write("Starting");
+  Wire.endTransmission();
+  delay(5000);
 }
 
 void loop() {
@@ -12,8 +29,7 @@ void loop() {
   // These next two bytes send the command to clear the display.
   Wire.write(254);
   Wire.write(88);
+  Wire.write("Temperature:");
   Wire.endTransmission();
   delay(500);
 }
-
-
